d28fb905ccae7176e78c96529eb2d5a2e3e9ca46
kjqw <johnson0@keio.jp>
2024-08-29 14:23:15 +0900

    xpathにできるだけ i1,i2, ... が入らないように、相対パスとして記述できるところは相対パスにした

--- scraping/xpath.py
+++ scraping/xpath.py
@@ -14,36 +14,49 @@ XPATH_TOTAL_COMMENT_COUNT_WITHOUT_REPLY = (
 )
 
 # 専門家コメントの要素
-XPATH_EXPERT_USERNAME = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[1]/li[i1]/article/div[2]/div[1]/h2/a"
-XPATH_EXPERT_POSTED_TIME = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[1]/li[i1]/article/div[2]/div[1]/time/a"
-XPATH_EXPERT_TYPE = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[1]/li[i1]/article/div[2]/div[1]/p"
-XPATH_EXPERT_COMMENT_BUTTON = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[1]/li[i1]/article/div[2]/p/button"
-XPATH_EXPERT_COMMENT_TEXT = (
-    "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[1]/li[i1]/article/div[2]/p"
+XPATH_EXPERT_COMMENT_SECTIONS = (
+    "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[1]/li[i1]"
 )
-XPATH_EXPERT_AGREEMENTS = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[1]/li[i1]/article/div[2]/div[2]/button/span"
+RELATIVE_XPATH_EXPERT_USERNAME = "article/div[2]/div[1]/h2/a"
+RELATIVE_XPATH_EXPERT_POSTED_TIME = "article/div[2]/div[1]/time/a"
+RELATIVE_XPATH_EXPERT_TYPE = "article/div[2]/div[1]/p"
+RELATIVE_XPATH_EXPERT_COMMENT_BUTTON = "article/div[2]/p/button"
+RELATIVE_XPATH_EXPERT_COMMENT_TEXT = "article/div[2]/p"
+RELATIVE_XPATH_EXPERT_AGREEMENTS = "article/div[2]/div[2]/button/span"
 
 # 一般コメントの要素
 XPATH_GENERAL_COMMENT_SECTIONS = (
     "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]"
 )
-XPATH_GENERAL_COMMENT_USERNAME = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]/article/article/div[2]/div[1]/h2/a"
-XPATH_GENERAL_COMMENT_POSTED_TIME = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]/article/article/div[2]/div[1]/time/a"
-XPATH_GENERAL_COMMENT_COMMENT_TEXT = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]/article/article/div[2]/p"
-XPATH_GENERAL_COMMENT_REPLY_AGREEMENTS = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]/article/article/div[2]/div[2]/div/ul/li[1]/button[2]/span/span[2]"
-XPATH_GENERAL_COMMENT_REPLY_ACKNOWLEDGEMENTS = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]/article/article/div[2]/div[2]/div/ul/li[2]/button[2]/span/span[2]"
-XPATH_GENERAL_COMMENT_REPLY_DISAGREEMENTS = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]/article/article/div[2]/div[2]/div/ul/li[3]/button[2]/span/span[2]"
-XPATH_GENERAL_COMMENT_REPLY_COUNT = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]/article/article/div[2]/div[2]/button[1]/span"
-XPATH_GENERAL_COMMENT_REPLY_BUTTON = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]/article/article/div[2]/div[2]/button[1]"
-XPATH_GENERAL_COMMENT_REPLY_BUTTON_MORE = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]/div/div[1]/div/button"
+RELATIVE_XPATH_GENERAL_COMMENT_USERNAME = "article/article/div[2]/div[1]/h2/a"
+RELATIVE_XPATH_GENERAL_COMMENT_POSTED_TIME = "article/article/div[2]/div[1]/time/a"
+RELATIVE_XPATH_GENERAL_COMMENT_COMMENT_TEXT = "article/article/div[2]/p"
+RELATIVE_XPATH_GENERAL_COMMENT_REPLY_AGREEMENTS = (
+    "article/article/div[2]/div[2]/div/ul/li[1]/button[2]/span/span[2]"
+)
+RELATIVE_XPATH_GENERAL_COMMENT_REPLY_ACKNOWLEDGEMENTS = (
+    "article/article/div[2]/div[2]/div/ul/li[2]/button[2]/span/span[2]"
+)
+RELATIVE_XPATH_GENERAL_COMMENT_REPLY_DISAGREEMENTS = (
+    "article/article/div[2]/div[2]/div/ul/li[3]/button[2]/span/span[2]"
+)
+RELATIVE_XPATH_GENERAL_COMMENT_REPLY_COUNT = (
+    "article/article/div[2]/div[2]/button[1]/span"
+)
+RELATIVE_XPATH_GENERAL_COMMENT_REPLY_BUTTON = "article/article/div[2]/div[2]/button[1]"
+RELATIVE_XPATH_GENERAL_COMMENT_REPLY_BUTTON_MORE = "div/div[1]/div/button"
 
 # 返信コメントの要素
-XPATH_REPLY_COMMENT_SECTIONS = (
-    "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]/div/ul/li[i3]"
+XPATH_REPLY_COMMENT_SECTIONS = f"{XPATH_GENERAL_COMMENT_SECTIONS}/div/ul/li[i3]"
+RELATIVE_XPATH_REPLY_COMMENT_USERNAME = "div/article/div[2]/div[1]/h2/a"
+RELATIVE_XPATH_REPLY_COMMENT_POSTED_TIME = "div/article/div[2]/div[1]/time"
+RELATIVE_XPATH_REPLY_COMMENT_COMMENT_TEXT = "div/article/div[2]/p"
+RELATIVE_XPATH_REPLY_COMMENT_AGREEMENTS = (
+    "div/article/div[2]/div[2]/ul/li[1]/button[2]/span/span[2]"
 )
-XPATH_REPLY_COMMENT_USERNAME = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]/div/ul/li[i3]/div/article/div[2]/div[1]/h2/a"
-XPATH_REPLY_COMMENT_POSTED_TIME = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]/div/ul/li[i3]/div/article/div[2]/div[1]/time"
-XPATH_REPLY_COMMENT_COMMENT_TEXT = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]/div/ul/li[i3]/div/article/div[2]/p"
-XPATH_REPLY_COMMENT_AGREEMENTS = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]/div/ul/li[i3]/div/article/div[2]/div[2]/ul/li[1]/button[2]/span/span[2]"
-XPATH_REPLY_COMMENT_ACKNOWLEDGEMENTS = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]/div/ul/li[i3]/div/article/div[2]/div[2]/ul/li[2]/button[2]/span/span[2]"
-XPATH_REPLY_COMMENT_DISAGREEMENTS = "/html/body/div[1]/div/main/div[1]/div[1]/article/ul[2]/li[i2]/div/ul/li[i3]/div/article/div[2]/div[2]/ul/li[3]/button[2]/span/span[2]"
+RELATIVE_XPATH_REPLY_COMMENT_ACKNOWLEDGEMENTS = (
+    "div/article/div[2]/div[2]/ul/li[2]/button[2]/span/span[2]"
+)
+RELATIVE_XPATH_REPLY_COMMENT_DISAGREEMENTS = (
+    "div/article/div[2]/div[2]/ul/li[3]/button[2]/span/span[2]"
+)
