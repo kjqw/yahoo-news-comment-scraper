c3d9e5f73f02aeabaa27ff3c386f8db483ab35b5
kjqw <johnson0@keio.jp>
2024-08-29 22:36:35 +0900

    全コメントを取得する関数を作成

--- scraping/article_comment_scraper.py
+++ scraping/article_comment_scraper.py
@@ -188,6 +188,34 @@ def get_article_comments(
     """
 
     try:
+        # スクレイピングの結果を格納する辞書を初期化
+        data = defaultdict(str)
+
+        # XPathの辞書を作成
+        xpaths_article = {
+            "article_title": XPATH_ARTICLE_TITLE,
+            "article_author": XPATH_ARTICLE_AUTHOR,
+            "article_posted_time": XPATH_ARTICLE_POSTED_TIME,
+            "total_comment_count_with_reply": XPATH_TOTAL_COMMENT_COUNT_WITH_REPLY,
+            "total_comment_count_without_reply": XPATH_TOTAL_COMMENT_COUNT_WITHOUT_REPLY,
+        }
+        xpaths_general_comments = {
+            "username": RELATIVE_XPATH_GENERAL_COMMENT_USERNAME,
+            "posted_time": RELATIVE_XPATH_GENERAL_COMMENT_POSTED_TIME,
+            "comment_text": RELATIVE_XPATH_GENERAL_COMMENT_COMMENT_TEXT,
+            "agreements": RELATIVE_XPATH_GENERAL_COMMENT_AGREEMENTS,
+            "acknowledgements": RELATIVE_XPATH_GENERAL_COMMENT_ACKNOWLEDGEMENTS,
+            "disagreements": RELATIVE_XPATH_GENERAL_COMMENT_DISAGREEMENTS,
+            "reply_count": RELATIVE_XPATH_GENERAL_COMMENT_REPLY_COUNT,
+        }
+        xpaths_reply_comments = {
+            "username": RELATIVE_XPATH_REPLY_COMMENT_USERNAME,
+            "posted_time": RELATIVE_XPATH_REPLY_COMMENT_POSTED_TIME,
+            "comment_text": RELATIVE_XPATH_REPLY_COMMENT_COMMENT_TEXT,
+            "agreements": RELATIVE_XPATH_REPLY_COMMENT_AGREEMENTS,
+            "acknowledgements": RELATIVE_XPATH_REPLY_COMMENT_ACKNOWLEDGEMENTS,
+            "disagreements": RELATIVE_XPATH_REPLY_COMMENT_DISAGREEMENTS,
+        }
 
         # ドライバを初期化
         driver = utils.init_driver(timeout)
@@ -195,19 +223,67 @@ def get_article_comments(
         # 記事のコメントページを開く
         driver.get(url)
 
-        # コメントセクションのリストを取得
-        comment_sections = utils.get_comment_sections(driver)
+        # 記事の情報を取得
+        for key, xpath in xpaths_article.items():
+            data[key] = driver.find_element(By.XPATH, xpath).text
 
-        data = defaultdict(str)
-        data["article_title"] = driver.find_element(By.XPATH, XPATH_ARTICLE_TITLE).text
+        # 最大数に達するまでコメントを取得
+        page = 1
+        data["comments"] = []
+        while (page - 1) * 10 < max_comments:
+            # 一般コメントのセクションを取得
+            general_comment_sections = utils.get_comment_sections(driver)
+            # それぞれの一般コメントについて処理
+            for comment_section in general_comment_sections:
+                # 返信コメントのセクションを取得
+                reply_comment_sections = get_reply_comment_sections(
+                    comment_section, max_replies
+                )
+
+                # 一般コメントの情報を取得
+                tmp_dict = {}
+                for key, xpath in xpaths_general_comments.items():
+                    tmp_dict[key] = comment_section.find_element(By.XPATH, xpath).text
+
+                # 一般コメントのオブジェクトを作成
+                general_comment = GeneralComment(
+                    username=tmp_dict["username"],
+                    posted_time=tmp_dict["posted_time"],
+                    comment_text=tmp_dict["comment_text"],
+                    agreements=tmp_dict["agreements"],
+                    acknowledgements=tmp_dict["acknowledgements"],
+                    disagreements=tmp_dict["disagreements"],
+                    reply_count=tmp_dict["reply_count"],
+                )
+
+                # 返信コメントの情報を取得
+                for reply_comment_section in reply_comment_sections:
+                    tmp_dict = {}
+                    for key, xpath in xpaths_reply_comments.items():
+                        tmp_dict[key] = reply_comment_section.find_element(
+                            By.XPATH, xpath
+                        ).text
+
+                    # 返信コメントのオブジェクトを作成
+                    reply_comment = ReplyComment(
+                        username=tmp_dict["username"],
+                        posted_time=tmp_dict["posted_time"],
+                        comment_text=tmp_dict["comment_text"],
+                        agreements=tmp_dict["agreements"],
+                        acknowledgements=tmp_dict["acknowledgements"],
+                        disagreements=tmp_dict["disagreements"],
+                    )
+                    general_comment.reply_comments.append(reply_comment)
+                    reply_comment.set_base_comment(general_comment)
+
+                # 一般コメントを格納
+                data["comments"].append(general_comment)
+
+            # 次のページに移動
+            page += 1
+            utils.open_page(driver, url, page, order)
 
         return data
-
-        for comment_section in comment_sections:
-            # 返信コメントのセクションを取得
-            reply_comment_sections = get_reply_comment_sections(
-                comment_section, max_replies
-            )
 
     finally:
         driver.quit()
