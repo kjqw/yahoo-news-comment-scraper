58154d35fe53d92efd3f47b8db5df20cb47a36c5
kjqw <johnson0@keio.jp>
2024-11-20 13:49:19 +0900

    change: カテゴリとポジネガを独立に分類するようにした

--- scripts/llm/zeroshot.py
+++ scripts/llm/zeroshot.py
@@ -3,36 +3,61 @@ import torch
 from transformers import pipeline
 
 # %%
+# 使用するデバイス
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device
+print(device)
 
 # %%
-# model_name = "MoritzLaurer/deberta-v3-large-zeroshot-v2.0"
-# text = "Angela Merkel is a politician in Germany and leader of the CDU"
-# labels = ["politics", "economy", "entertainment", "environment"]
-# hypothesis_template = "This text is about {}"
-
+# モデルと入力データ
 model_name = "MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7"
-text = ["今日、新しいiPhoneが発売されました"]
-labels = ["スマートフォン", "エンタメ", "スポーツ"]
-hypothesis_template = "このニュースは{}に関する文章です."
+texts = [
+    "新しいiPhoneの価格が高すぎると批判されています。",
+    "日本代表がワールドカップで優勝しました！",
+    "映画のレビューが酷評されていました。",
+]
+labels_category = ["スマートフォン", "エンタメ", "スポーツ"]
+labels_sentiment = ["ネガティブ", "ポジティブ"]
+
+# プロンプトテンプレート
+# hypothesis_template = "この文章は{}に関する内容で、{}です。"
+hypothesis_template_category = "この文章は{}に関する内容です。"
+hypothesis_template_sentiment = "この文章の感情は{}です。"
 
 # %%
-zeroshot_classifier = pipeline(
-    "zero-shot-classification",
-    model=model_name,
-    device=device,
-)
-output = zeroshot_classifier(
-    text, labels, hypothesis_template=hypothesis_template, multi_label=False
-)
+try:
+    # モデルロード
+    zeroshot_classifier = pipeline(
+        "zero-shot-classification",
+        model=model_name,
+        device=0 if device.type == "cuda" else -1,
+    )
 
-# %%
-print(output)
+    # ゼロショット分類
+    results_category = zeroshot_classifier(
+        texts,
+        labels_category,
+        hypothesis_template=hypothesis_template_category,
+        # multi_label=False,
+    )
+    results_sentiment = zeroshot_classifier(
+        texts,
+        labels_sentiment,
+        hypothesis_template=hypothesis_template_sentiment,
+        multi_label=False,
+    )
+
+except Exception as e:
+    print(f"エラーが発生しました: {e}")
 
 # %%
 # メモリ解放
 del zeroshot_classifier
 torch.cuda.empty_cache()
+
+# %%
+results_category
+
+# %%
+results_sentiment
 
 # %%
