2226c26e47a4c2d8daa51badc60a20abe7635eb2
kjqw <johnson0@keio.jp>
2024-11-21 03:55:26 +0900

    add

--- scripts/scraping/main.py
+++ scripts/scraping/main.py
@@ -12,6 +12,8 @@ from pathlib import Path
 
 import article_comment_scraper
 import article_link_scraper
+import user_comment_scraper
+from tqdm import tqdm
 
 sys.path.append(str(Path(__file__).parents[1]))
 
@@ -19,7 +21,6 @@ from db_manager import execute_query
 
 # %%
 # データベースの初期化
-# %%
 db_config = {
     "host": "postgresql_db",
     "database": "yahoo_news",
@@ -33,12 +34,13 @@ with init_sql_path.open() as f:
 execute_query(query, db_config=db_config, commit=True)
 
 # %%
-article_link_scraper.get_and_save_articles()
-# %%
 default_max_comments = 30
 default_max_replies = 10
 default_max_articles = 10
 default_timeout = 10
+
+# %%
+article_link_scraper.get_and_save_articles()
 
 # %%
 # 記事のリンクを取得
@@ -72,6 +74,28 @@ for article_id, article_comment_link in article_comment_links:
         default_max_comments,
         default_max_replies,
         timeout=default_timeout,
+    )
+
+# %%
+user_links = [
+    i[0]
+    for i in execute_query(
+        query=f"""
+        SELECT user_link
+        FROM comments
+        WHERE agreements_count IS NOT NULL
+        GROUP BY user_link
+        ORDER BY MAX(agreements_count) DESC
+        LIMIT 10;
+        """,
+        db_config=db_config,
+    )
+]
+for user_link in tqdm(user_links):
+    user_comment_scraper.get_and_save_articles_and_comments(
+        db_config,
+        user_link,
+        default_max_comments,
     )
 
 # %%
