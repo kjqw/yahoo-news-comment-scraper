05be5aa307f763bb58df8e593887a47e907a3df4
kjqw <johnson0@keio.jp>
2024-12-19 12:08:15 +0900

    change: REFERENCESがあるとエラーが何度も起きたので消去

--- scripts/model/real/init.sql
+++ scripts/model/real/init.sql
@@ -1,7 +1,7 @@
 -- ユーザーの遷移モデルの学習データ用のテーブルを作成する
 
 CREATE TABLE IF NOT EXISTS articles_llm_output (
-    article_id INTEGER REFERENCES articles (article_id),
+    article_id INTEGER,
     article_title TEXT,
     -- article_title_vector FLOAT[],
     article_content TEXT,
@@ -12,30 +12,26 @@ CREATE TABLE IF NOT EXISTS articles_llm_output (
 );
 
 CREATE TABLE IF NOT EXISTS comments_llm_output (
-    comment_id INTEGER REFERENCES comments (comment_id),
-    article_id INTEGER REFERENCES articles_llm_output (article_id),
-    parent_comment_id INTEGER REFERENCES comments_llm_output (comment_id),
-    user_id INTEGER REFERENCES users (user_id),
+    comment_id INTEGER,
+    article_id INTEGER,
+    parent_comment_id INTEGER,
+    user_id INTEGER,
     posted_time TEXT,
     normalized_posted_time TIMESTAMP,
     is_time_uncertain BOOLEAN,
     comment_content TEXT,
     comment_content_vector FLOAT[],
-    UNIQUE (
-        comment_id,
-        article_id,
-        user_id,
-        comment_content
-    )
+    UNIQUE (comment_id)
 );
 
 CREATE TABLE IF NOT EXISTS training_data (
-    user_id INTEGER REFERENCES users (user_id),
-    article_id INTEGER REFERENCES articles_llm_output (article_id),
-    article_content_vector FLOAT[] REFERENCES articles_llm_output (article_content_vector),
-    parent_comment_id INTEGER REFERENCES comments_llm_output (comment_id),
-    parent_comment_content_vector FLOAT[] REFERENCES comments_llm_output (comment_content_vector),
-    comment_id INTEGER REFERENCES comments_llm_output (comment_id),
-    comment_content_vector FLOAT[] REFERENCES comments_llm_output (comment_content_vector),
-    normalized_posted_time TIMESTAMP REFERENCES comments_llm_output (normalized_posted_time)
+    user_id INTEGER,
+    article_id INTEGER,
+    article_content_vector FLOAT[],
+    parent_comment_id INTEGER,
+    parent_comment_content_vector FLOAT[],
+    comment_id INTEGER,
+    comment_content_vector FLOAT[],
+    normalized_posted_time TIMESTAMP,
+    UNIQUE (user_id, article_id, parent_comment_id, comment_id)
 );
