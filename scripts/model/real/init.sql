ff50527eb9e13ea7c7c1871b846d06bb74092fa4
kjqw <johnson0@keio.jp>
2025-01-07 13:42:52 +0900

    add: ベクトル化したものの保存先を分けた

--- scripts/model/real/init.sql
+++ scripts/model/real/init.sql
@@ -10,7 +10,19 @@ CREATE TABLE IF NOT EXISTS training_data_raw (
     UNIQUE (comment_id)
 );
 
-CREATE TABLE IF NOT EXISTS training_data_vectorized (
+CREATE TABLE IF NOT EXISTS training_data_vectorized_sentiment (
+    user_id INTEGER,
+    article_id INTEGER,
+    article_content_vector FLOAT[],
+    parent_comment_id INTEGER,
+    parent_comment_content_vector FLOAT[],
+    comment_id INTEGER,
+    comment_content_vector FLOAT[],
+    normalized_posted_time TIMESTAMP,
+    UNIQUE (comment_id)
+);
+
+CREATE TABLE IF NOT EXISTS training_data_vectorized_category (
     user_id INTEGER,
     article_id INTEGER,
     article_content_vector FLOAT[],
